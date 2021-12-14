ddb5887f75d2edd9ca40e149073c0c80cab25456
AndreCioffi <andre.cioffi97@gmail.com>
2021-12-14 10:34:04 +0100

    declaration started

---
+++ src/FC_Lambda.h
@@ -0,0 +1,14 @@
+#ifndef __FC_LAMBDA_H__
+#define __FC_LAMBDA_H__
+
+#include "FullConditional.h"
+
+class FC_Lambda : public FullConditional{
+private:
+    /* data */
+public:
+    FC_Lambda : public FullConditional(/* args */);
+    ~FC_Lambda : public FullConditional();
+};
+
+#endif
