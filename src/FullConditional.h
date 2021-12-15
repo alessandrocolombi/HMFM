9af4b7172d12610c3b1d99d5b0a06f19f7f53c92
AndreCioffi <andre.cioffi97@gmail.com>
2021-12-15 12:45:41 +0100

    adapted to GS_data

--- src/FullConditional.h
+++ src/FullConditional.h
@@ -1,10 +1,10 @@
 #include "include_headers.h"
-#include "GDFMM_types.h"
+#include "GS_data.h"
 #include "GSL_wrappers.h"
 
 class FullConditional{
     
 public :
-    virtual void update(GS_parameters& param, sample::GSL_RNG engine) = 0;
+    virtual void update(GS_data& gs_data, sample::GSL_RNG gs_engine) = 0;
     bool binary_decision(double p1, sample::GSL_RNG engine);
 };
