7e10c5216a2d990fc44174a9b2bceda158a77239
PitBosch <62173411+PitBosch@users.noreply.github.com>
2021-12-17 17:04:05 +0100

    update fc tau (minor changes)

--- src/FC_tau.cpp
+++ src/FC_tau.cpp
@@ -14,39 +14,44 @@ void FC_tau::update(GS_data& gs_data, sample::GSL_RNG gs_engine, const string &c
     std::vector<unsigned int> ind_i; // i index of C elements
     std::vector<unsigned int> ind_j;// j index of C elements
     std::vector< std::vector<unsigned int>> Ctilde; // matrix of partition 
-
+    std::vector<std::vector<double>> data=gs_data.data; //matrix of data
     if (c == 'normal-inverse-gamma') {
-        //tau-nonallocate
+        
+        
         sample::rgamma Gamma;
         sample::rnorm rnorm;
+        
+        //tau-nonallocate
+        
         for (int m=0;m<Mna;++m){
-             float sigma2_na=1 / Gamma(gs_engine, nu_0/2, (nu_0)*((sigma_0)/2)); // vardellecomponenti
-             float mu_na=rnorm( mu_0, std::sqrt(sigma2_na / k_0)); // mediadellecomponenti
+             float sigma2_na=1 / Gamma(gs_engine, nu_0/2, (nu_0)*((sigma_0)/2)); // Non allocated Components' variance
+             float mu_na=rnorm( mu_0, std::sqrt(sigma2_na / k_0)); // Non allocated Components' mean
 
-             gs_data.mu.pushback(mu_na);
-             gs_data.sigma.pushback(sigma2_na);
+             gs_data.mu.push_back(mu_na);
+             gs_data.sigma.push_back(sigma2_na);
         }
+        
+        
          //tau-allocate
+        
         for (int k= 0; k <K; ++k) {
             for (int j = 0; j <d ; ++j) {
                 for (int i = 0; i <n_j[j] ; ++i) {
                     if(C[j][i]==clust_out[k]){
                         N(j,k)++;
-                        ind_i.pushback(i);
-                        ind_j.pushback(j);
+                        ind_i.push_back(i);
+                        ind_j.push_back(j);
                         Ctilde[j][k]=k;
                     }
                 }
-            }
 //set Ctilde in partition 
             nu_n_clust = nu_0 + N_k[k];
-
             lpk = k_0 + N_k[k];
-            y_bar_clust= mean(ind_i,ind_j);
-            s2_clust= var(mean,ind_i, ind_j);
+            y_bar_clust= mean(ind_i, ind_j);
+            s2_clust= var(y_bar_clust, ind_i, ind_j);
 //if (is.na(s2_clust[k])){s2_clust[k] <- 0}
             mu_n_clust = (k_0 * mu_0 + N_k[k] * y_bar_clust) / lpk;
-            sigma2_n_clust = (nu_0 * (sigma_0 ^ 2) + (N_k[k] - 1) * s2_clust+ k_0 * N_k[k]  *(y_bar_clust - mu_0) ^ 2 / (lpk));
+            sigma2_n_clust = (nu_0 * (sigma_0 * sigma_0) + (N_k[k] - 1) * s2_clust+ k_0 * N_k[k] * (y_bar_clust - mu_0) * (y_bar_clust - mu_0) / (lpk));
 
             //Campionamento
             sigma2_a = 1 / rgamma(gs_engine, nu_n_clust/ 2, sigma2_n_clust / 2);
@@ -60,21 +65,21 @@ void FC_tau::update(GS_data& gs_data, sample::GSL_RNG gs_engine, const string &c
 }
 }
 
-double mean(std::vector<unsigned int> ind_i,std::vector<unsigned int>ind_j){
+double mean(std::vector<unsigned int> ind_i, std::vector<unsigned int> ind_j){
     int count=0;
     double sum=0;
-    for (int m = 0; m <ind_i ; ++m) {
-        sum+=gs_data:data[ind_j[m]][ind_i[m]];
+    for (int m = 0; m <ind_i.size(); ++m) {
+        sum+=data[ind_j[m]][ind_i[m]];
         count++;
 
     }
     return sum/count;
 }
  double var(double mean,std::vector<unsigned int> ind_i,std::vector<unsigned int>ind_j){
-    double var
-    int count=0
-     for (int m = 0; m <ind_i ; ++m) {
-         var+=(gs_data:data[ind_j[m]][ind_i[m]] - mean) * (gs_data:data[ind_j[m]][ind_i[m]] - mean)) ;
+    double var;
+    int count=0;
+     for (int m = 0; m <ind_i.size() ; ++m) {
+         var+=(data[ind_j[m]][ind_i[m]] - mean) * (data[ind_j[m]][ind_i[m]] - mean)) ;
          count++;
      }
      return var/count;
