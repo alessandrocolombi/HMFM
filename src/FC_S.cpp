f9b5028b3d52a8adb45fcef3d5643b3ccffe30b7
AndreCioffi <andreaisidoro.cioffi@mail.polimi.it>
2022-01-02 19:27:07 +0100

    correzione sampler

--- src/FC_S.cpp
+++ src/FC_S.cpp
@@ -16,13 +16,13 @@ void FC_S::update(GS_data& gs_data, const sample::GSL_RNG& gs_engine){
     for (unsigned j=0; j<d; j++) { //per ogni livello
         //S ALLOCATE
         for (unsigned k=0; k<K; k++) {//per ogni comp allocata
-            S(j, k) = Gamma(1, N(j, k) + gamma[j], U[j] + 1);
+            S(j, k) = Gamma(gs_engine, N(j, k) + gamma[j], U[j] + 1);
         }
 
         //S NON ALLOCATE
         if (Mstar > 0) { // se c'è almeno una componente non allocata
             for (unsigned mstar=0; mstar<Mstar; mstar++) {
-                S(j, mstar) = Gamma(1, gamma[j], U[j] + 1);
+                S(j, mstar) = Gamma(gs_engine, gamma[j], U[j] + 1);
             }
         }
     }
