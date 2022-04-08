8027ba9b19c5f7801c2792f26456dc6893d2b6ea
Alessandro Colombi <colombi.alessandro@hotmail.it>
2022-04-08 10:38:58 +0200

    minor bug fixed

--- src/FC_Lambda.h
+++ src/FC_Lambda.h
@@ -9,9 +9,9 @@ private:
     double a2 = 1;
     double b2 = 1;
 public:
-    FC_Lambda(std::string na, double a, double b, bool _keepfixed) : FullConditional(na,_keepfixed), a2(a), b2(b){}//, keep_fixed(_keepfixed){name = na;};
+    FC_Lambda(std::string na, double a, double b, bool _keepfixed) : FullConditional(na,_keepfixed), a2(a), b2(b){};
     FC_Lambda(std::string na, bool _keepfixed): FullConditional(na,_keepfixed){};
-    FC_Lambda(bool _keepfixed):FullConditional("Lambda",_keepfixed){};//keep_fixed(_keepfixed){name = "Lambda";};
+    FC_Lambda(bool _keepfixed):FullConditional("Lambda",_keepfixed){};
     ~FC_Lambda() {};
     void update(GS_data& gs_data, const sample::GSL_RNG& gs_engine) override;
 };
