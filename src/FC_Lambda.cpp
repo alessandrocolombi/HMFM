4e34460ad19054093392db4f7c1e3631042c475e
Alessandro Colombi <colombi.alessandro@hotmail.it>
2022-12-09 15:20:46 +0100

    fixed error in full conditional formula (tolto d ad esponente)

--- src/FC_Lambda.cpp
+++ src/FC_Lambda.cpp
@@ -12,15 +12,18 @@ void FC_Lambda::update(GS_data& gs_data, const sample::GSL_RNG& gs_engine){
     sample::rgamma Gamma;
 
     // UPDATE ROUTINE
-    double a2_star = static_cast<double>( d*(k-1) ) + a2;
+    double a2_star = static_cast<double>( (k-1) ) + a2;
+    //OLD: double a2_star = static_cast<double>( d*(k-1) ) + a2; // by mistake, I added that ^d in the peppf formula
 
     // Computation of the weight for the "first" gamma distr.
-    double p0 = (a2_star)/((a2_star-k)+k*(b2+1)*exp(log_sum));
+    double p0 = (a2_star)/( (a2_star-1.0) +
+                            (double)k * (b2+1.0)*exp(log_sum)
+                           );
     // Select, via extraction from a uniform, which distribution sample from
     bool select_p0 = binary_decision(p0, gs_engine);
 
     if(select_p0)
-        gs_data.lambda = Gamma(gs_engine, a2_star + 1, 1 /(b2 + 1 - exp(-log_sum)) );
+        gs_data.lambda = Gamma(gs_engine, a2_star + 1.0, 1.0 /(b2 + 1.0 - exp(-log_sum)) );
     else
-        gs_data.lambda = Gamma(gs_engine, a2_star, 1 /(b2 + 1 - exp(-log_sum)) );
+        gs_data.lambda = Gamma(gs_engine, a2_star, 1.0 /(b2 + 1.0 - exp(-log_sum)) );
 }
