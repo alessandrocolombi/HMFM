d6c78d1e91c4d3f19e97c414f12ca08fbd0b3cfa
Alessandro Colombi <colombi.alessandro@hotmail.it>
2023-05-28 15:27:55 +0200

    minor change

--- src/FC_PartitionMarginal.cpp
+++ src/FC_PartitionMarginal.cpp
@@ -213,14 +213,13 @@ void FC_PartitionMarginal::update(GS_data& gs_data, const sample::GSL_RNG& gs_en
                                                                         (location_prior - sum_cluster_elements[l]/(double)N_k[l] ) * (location_prior - sum_cluster_elements[l]/(double)N_k[l] )
                                                                 );
 
-                        scale_post = std::sqrt(sigma_0_post*(k_0_post + 1.0)/k_0_post); 
-                        //scale_post = std::sqrt(sigma_0_post*(k_0_post)/(k_0_post+1.0));
-                        // Dai conti teorici mi sembra ci voglia la riga sopra ma in pratica con quella sotto va molto meglio
+                        scale_post = std::sqrt(sigma_0_post) * std::sqrt( (k_0_post + 1.0)/k_0_post ); 
 
                                 //Rcpp::Rcout<<"gs_data.compute_var_in_cluster("<<l<<") = "<<gs_data.compute_var_in_cluster(l)<<std::endl;
                                 //Rcpp::Rcout<<"sigma_0_post = "<<sigma_0_post<<std::endl;
                                 //Rcpp::Rcout<<"k_0_post = "<<k_0_post<<std::endl;
                                 //Rcpp::Rcout<<"scale_post = "<<scale_post<<std::endl;
+
                         log_probs_vec[l] += log_dnct(data[j][i], dof_post, location_post, scale_post); // add likelihood term                  
                     //}
 
