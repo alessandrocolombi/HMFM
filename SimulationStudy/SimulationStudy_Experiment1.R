c4d308f8149e5f013b4e09b35b414ede278cf73b
Alessandro Colombi <colombi.alessandro@hotmail.it>
2024-07-16 18:48:56 +0200

    save results of experiment 1

--- SimulationStudy/SimulationStudy_Experiment1.R
+++ SimulationStudy/SimulationStudy_Experiment1.R
@@ -855,48 +855,6 @@ tictoc::tic()
   parallel::stopCluster(cluster)
 tictoc::toc()
 
-# Table group 1
-HDP_res = sapply(res, function(x){x$HDP$ARI_est_part_group1})
-HMFMmarg_res = sapply(res, function(x){x$HMFM_marg$ARI_est_part_group1})
-HMFMcond_res = sapply(res, function(x){x$HMFM_cond$ARI_est_part_group1})
-MFM_res = sapply(res, function(x){x$MFM$ARI_est_part_group1})
-pooled_res = sapply(res, function(x){x$pooled$ARI_est_part_group1})
-
-cat("\n GROUP 1 \n")
-cat("\n","HDP ARI: mean = ",mean(HDP_res),"; sd = ",sd(HDP_res),"\n")
-cat("\n","HMFM-marg ARI: mean = ",mean(HMFMmarg_res),"; sd = ",sd(HMFMmarg_res),"\n")
-cat("\n","HMFM-cond ARI: mean = ",mean(HMFMcond_res),"; sd = ",sd(HMFMcond_res),"\n")
-cat("\n","MFM ARI: mean = ",mean(MFM_res),"; sd = ",sd(MFM_res),"\n")
-cat("\n","pooled ARI: mean = ",mean(pooled_res),"; sd = ",sd(pooled_res),"\n")
-
-# Table group 2
-HDP_res = sapply(res, function(x){x$HDP$ARI_est_part_group2})
-HMFMmarg_res = sapply(res, function(x){x$HMFM_marg$ARI_est_part_group2})
-HMFMcond_res = sapply(res, function(x){x$HMFM_cond$ARI_est_part_group2})
-MFM_res = sapply(res, function(x){x$MFM$ARI_est_part_group2})
-pooled_res = sapply(res, function(x){x$pooled$ARI_est_part_group2})
-
-cat("\n GROUP 2 \n")
-cat("\n","HDP ARI: mean = ",mean(HDP_res),"; sd = ",sd(HDP_res),"\n")
-cat("\n","HMFM-marg ARI: mean = ",mean(HMFMmarg_res),"; sd = ",sd(HMFMmarg_res),"\n")
-cat("\n","HMFM-cond ARI: mean = ",mean(HMFMcond_res),"; sd = ",sd(HMFMcond_res),"\n")
-cat("\n","MFM ARI: mean = ",mean(MFM_res),"; sd = ",sd(MFM_res),"\n")
-cat("\n","pooled ARI: mean = ",mean(pooled_res),"; sd = ",sd(pooled_res),"\n")
-
-
-# 1 Cluster %
-HDP_res = sum(sapply(res, function(x){x$HDP$K2_ARI == 1}))/Nrep
-HMFMmarg_res = sum(sapply(res, function(x){x$HMFM_marg$K2_ARI == 1}))/Nrep
-HMFMcond_res = sum(sapply(res, function(x){x$HMFM_cond$K2_ARI == 1}))/Nrep
-MFM_res = sum(sapply(res, function(x){x$MFM$K2_ARI == 1}))/Nrep
-
-cat("\n % 1 cluster \n")
-cat("\n HDP : ",HDP_res * 100, "% \n")
-cat("\n HMFM-marg : ",HMFMmarg_res * 100, "% \n")
-cat("\n HMFM-cond : ",HMFMcond_res * 100, "% \n")
-cat("\n MFM : ",MFM_res * 100, "% \n")
-cat("\n pooled : NOT DEFINED \n")
-
 
 ## PS1
 name = "err_L1_group1"
@@ -942,6 +900,8 @@ PS2_plot1 = exp_temp %>% select(type,!!name) %>%
   labs(y=paste0(ylabel,", n = ",sum(n_j)), x = " ") + theme_bw() +
   theme(plot.title = element_text(hjust = 0.5), legend.position="none",
         text = element_text(size = 10))  + ylim(c(0,0.2))
+
+ggpubr::ggarrange(PS1_plot1, PS2_plot1, ncol = 2)
 
 ## CCE group 1
 name = "err_coclust_group1"
@@ -993,6 +953,8 @@ CCE2_plot1 = exp_temp %>% select(type,!!name) %>%
   theme(plot.title = element_text(hjust = 0.5), legend.position="none",
         text = element_text(size = 10))  + ylim(c(0,0.5))
 
+
+ggpubr::ggarrange(CCE1_plot1, CCE2_plot1, ncol = 2)
 
 ## K_ARI
 name = "K_ARI"
@@ -1049,5 +1011,49 @@ K_mode_plot1 = ggplot(exp_temp, aes(x = K_mode, fill = type)) +
         text = element_text(size = 10)) +
   labs(y=paste0(ylabel,", n = ",sum(n_j)), x = " ")
 # K_mode_plot1
+
+
+# Table group 1
+HDP_res = sapply(res, function(x){x$HDP$ARI_est_part_group1})
+HMFMmarg_res = sapply(res, function(x){x$HMFM_marg$ARI_est_part_group1})
+HMFMcond_res = sapply(res, function(x){x$HMFM_cond$ARI_est_part_group1})
+MFM_res = sapply(res, function(x){x$MFM$ARI_est_part_group1})
+pooled_res = sapply(res, function(x){x$pooled$ARI_est_part_group1})
+
+cat("\n GROUP 1 \n")
+cat("\n","HDP ARI: mean = ",mean(HDP_res),"; sd = ",sd(HDP_res),"\n")
+cat("\n","HMFM-marg ARI: mean = ",mean(HMFMmarg_res),"; sd = ",sd(HMFMmarg_res),"\n")
+cat("\n","HMFM-cond ARI: mean = ",mean(HMFMcond_res),"; sd = ",sd(HMFMcond_res),"\n")
+cat("\n","MFM ARI: mean = ",mean(MFM_res),"; sd = ",sd(MFM_res),"\n")
+cat("\n","pooled ARI: mean = ",mean(pooled_res),"; sd = ",sd(pooled_res),"\n")
+
+# Table group 2
+HDP_res = sapply(res, function(x){x$HDP$ARI_est_part_group2})
+HMFMmarg_res = sapply(res, function(x){x$HMFM_marg$ARI_est_part_group2})
+HMFMcond_res = sapply(res, function(x){x$HMFM_cond$ARI_est_part_group2})
+MFM_res = sapply(res, function(x){x$MFM$ARI_est_part_group2})
+pooled_res = sapply(res, function(x){x$pooled$ARI_est_part_group2})
+
+cat("\n GROUP 2 \n")
+cat("\n","HDP ARI: mean = ",mean(HDP_res),"; sd = ",sd(HDP_res),"\n")
+cat("\n","HMFM-marg ARI: mean = ",mean(HMFMmarg_res),"; sd = ",sd(HMFMmarg_res),"\n")
+cat("\n","HMFM-cond ARI: mean = ",mean(HMFMcond_res),"; sd = ",sd(HMFMcond_res),"\n")
+cat("\n","MFM ARI: mean = ",mean(MFM_res),"; sd = ",sd(MFM_res),"\n")
+cat("\n","pooled ARI: mean = ",mean(pooled_res),"; sd = ",sd(pooled_res),"\n")
+
+
+# 1 Cluster %
+HDP_res = sum(sapply(res, function(x){x$HDP$K2_ARI == 1}))/Nrep
+HMFMmarg_res = sum(sapply(res, function(x){x$HMFM_marg$K2_ARI == 1}))/Nrep
+HMFMcond_res = sum(sapply(res, function(x){x$HMFM_cond$K2_ARI == 1}))/Nrep
+MFM_res = sum(sapply(res, function(x){x$MFM$K2_ARI == 1}))/Nrep
+
+cat("\n % 1 cluster \n")
+cat("\n HDP : ",HDP_res * 100, "% \n")
+cat("\n HMFM-marg : ",HMFMmarg_res * 100, "% \n")
+cat("\n HMFM-cond : ",HMFMcond_res * 100, "% \n")
+cat("\n MFM : ",MFM_res * 100, "% \n")
+cat("\n pooled : NOT DEFINED \n")
+
 
 beepr::beep()
